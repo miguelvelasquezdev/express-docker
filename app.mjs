4166e46d37dda4940d6c8a61589eabe13392d783
Miguel Velasquez <veruiz.miguel@gmail.com>
2023-04-06 12:45:15 -0500

    first commit

---
+++ app.mjs
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const hostname = "127.0.0.1";
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
